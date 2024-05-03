b1bc1dbb2647de339b38b3fb59926b4fb6ffa3e5
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-05-03 00:01:23 -0300

    adicionando novos arquivos

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+$('.hero__carousel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    adaptativeHeight: true,
+    // variableWidth: true
+});
